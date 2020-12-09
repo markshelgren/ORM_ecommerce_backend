847c7aa03cf0cdc8d44e310d79d2bbcba6121c59
Mark Shelgren <markshelgren@outlook.com>
2020-12-09 12:09:04 -0800

    Defined all the models and the database relationships

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,20 +1,28 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-  }
+	{
+		// define columns
+		product_id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		tag_id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "product_tag",
+	}
 );
 
 module.exports = ProductTag;
