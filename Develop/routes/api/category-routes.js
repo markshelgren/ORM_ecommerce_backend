ca808d5df5a0782330e8b43ca69bd22e0ef07928
Mark Shelgren <markshelgren@outlook.com>
2020-12-09 12:29:30 -0800

    Changed the routes alot.  Needs lots of testing

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,28 +1,67 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get("/", (req, res) => {
+	// find all categories
+	// be sure to include its associated Products
+	Category.findAll({
+		include: [Product],
+	}).then((results) => {
+		res.json(results);
+	});
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get("/:id", (req, res) => {
+	// find one category by its `id` value
+	// be sure to include its associated Products
+	Category.findOne({
+		//can also use findAll
+		where: {
+			id: req.params.id,
+		},
+		include: [Product],
+	}).then((results) => {
+		res.json(results);
+	});
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post("/", (req, res) => {
+	// create a new category
+	Category.create({
+		id: req.body.id,
+		category_name: req.body.category_name,
+	}).then((results) => {
+		res.json(results);
+	});
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put("/:id", (req, res) => {
+	// update a category by its `id` value
+	Category.update(
+		{
+			category_name: req.body.category_name,
+		},
+		{
+			where: {
+				id: req.params.id,
+			},
+		}
+	).then((results) => {
+		res.json(results);
+	});
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete("/:id", (req, res) => {
+	// delete a category by its `id` value
+	Category.destroy({
+		where: {
+			id: req.params.id,
+		},
+	}).then((results) => {
+		res.json(results);
+	});
 });
 
 module.exports = router;
